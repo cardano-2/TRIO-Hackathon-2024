89fd68ea4055f05d9971011eb9c9fc174f8c1b76
cardano-2 <cardano.2@buckeyemail.osu.edu>
2024-10-19 10:03:34 -0400

    hjkl

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,6 @@ public class Test {
         System.out.println("Hello, World!");
         System.out.println("This is team Trio with Joselyn, Melvin, and Sharmaine");
         System.out.println("hi");
+        System.out.println("Hey yall");
     }
 }
